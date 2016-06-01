67cbabc1a50a57d6d032c90f3290de27fe9612ba
Jack Moffitt <jack@metajack.im>
2016-05-31 20:15:48 -0600

    WIP

--- build.rs
+++ build.rs
@@ -2,28 +2,24 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+extern crate cmake;
 extern crate pkg_config;
-extern crate make_cmd;
 
 use std::env;
 
 fn main() {
-    if pkg_config::Config::new().atleast_version("2.1.0").find("expat").is_ok()
-    {
-        return;
+    if pkg_config::Config::new().atleast_version("2.1.0").find("expat").is_ok() {
+        return
     }
 
-    assert!(make_cmd::make()
-        .args(&["-f", "makefile.cargo"])
-        .status()
-        .unwrap()
-        .success());
-    let out_dir = env::var("OUT_DIR").unwrap();
-    if env::var("TARGET").unwrap().contains("eabi") {
-        println!("cargo:rustc-link-search=native={}", out_dir);
+    //let dst = cmake::Config::new("expat").build_target("ALL_BUILD").build();
+    let mut dst = cmake::build("expat");
+    dst.push("lib");
+    let target = env::var("TARGET").unwrap();
+    if target.contains("eabi") || target.contains("windows") {
+        println!("cargo:rustc-link-search=native={}", dst.display());
         println!("cargo:rustc-link-lib=static=expat");
     } else {
         println!("cargo:rustc-link-lib=expat");
     }
-    println!("cargo:outdir={}", out_dir);
 }
